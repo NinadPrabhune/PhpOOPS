572fc6e052277a43b8886796573a93b1efbb549c
Ninad Prabhune <prabhuneninad@gmail.com>
2021-12-19 12:53:04 +0530

    first commit

---
+++ class&objects.php
@@ -0,0 +1,30 @@
+<?php
+class Calculation{
+
+    public $a,$b,$res1,$res2;   //prorerties
+    
+    function Sum(){             // methods / functions 1   
+        $res1=$this->a + $this->b;
+        return $res1;
+    }
+
+    function Sub(){             // methods / functions 2   
+        $res2=$this->a * $this->b;
+        return $res2;
+    }  
+}
+$cal1= new Calculation();        // creating object
+$cal1->a=10;
+$cal1->b=120;
+echo $cal1->Sum()."<br>";
+echo $cal1->Sub()."<br><br>";
+
+
+$cal2= new Calculation();        // creating object
+$cal2->a=1110;
+$cal2->b=12890;
+echo $cal2->Sum()."<br>";
+echo $cal2->Sub()."<br>";
+
+
+?>
