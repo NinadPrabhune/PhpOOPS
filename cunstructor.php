196284b3aa6de50cc604fb484341afba903cf865
Ninad Prabhune <prabhuneninad@gmail.com>
2021-12-19 13:19:26 +0530

    constructor created

---
+++ cunstructor.php
@@ -0,0 +1,30 @@
+<?php
+
+
+class Person{
+
+public $name;                                               //prorerties $name global scope
+public $degree;                                             //prorerties
+
+function __construct($name="No Name",$degree="Null")        //constructor $name local scope
+{
+   $this->name=$name; 
+   $this->degree=$degree; 
+}
+
+function Show(){                                            // methods / functions 1
+    echo "Your name     : ".$this->name."<br>";
+    echo "Your degree   : ".$this->degree."<br><br>";
+}
+}
+
+
+$per1= new Person();
+$per2= new Person('Ninad Prabhune','MCA');
+$per3= new Person('Alon mask','Bachelor of Science in Physics / Bachelor of Arts in Economics');
+
+$per1->Show();
+$per2->Show();
+$per3->Show();
+
+?>
