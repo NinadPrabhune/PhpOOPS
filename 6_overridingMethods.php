55c401378940804e0c49aca7363e31ac96197a4a
Ninad Prabhune <prabhuneninad@gmail.com>
2021-12-22 13:50:48 +0530

    commit 2

---
+++ 6_overridingMethods.php
@@ -0,0 +1,32 @@
+<?php
+
+// Overriding Methods :
+// Same name of method on both classes base or derived class.
+
+
+
+class base {
+    public $name ="<br> Parent Class <br>";//Same properties $name
+    public function calculation($a,$b){
+        return $a*$b;
+    }
+    
+}
+class derived extends base{
+    public $name ="<br> Child Class <br>"; //same properties $name
+    public function calculation($a,$b){
+        return $a+$b;
+    }  // if we run this command it gives 15 as a outupt 
+    
+}
+
+$find =new derived();
+echo $find->calculation(10,5); // derived class use
+
+$find=new base();
+echo $find->name; // base class use
+echo $find->calculation(10,5); // base class use
+
+
+
+?>
