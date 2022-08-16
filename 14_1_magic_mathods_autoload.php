1cd815336abd8568734a8e7bb90b8faac8b28117
Ninad Prabhune <prabhuneninad@gmail.com>
2022-08-16 16:15:33 +0530

    Magic Method added

---
+++ 14_1_magic_mathods_autoload.php
@@ -0,0 +1,29 @@
+<?php
+
+
+
+
+// Rule
+
+// require class name same as file name
+
+
+
+// require "classes/first.php";
+// require "classes/second.php";
+
+
+function __autoload($class){
+
+    require "classes/". $class .".php";
+
+}
+
+
+
+$test = new first();
+
+$test = new second();
+
+?>
+
