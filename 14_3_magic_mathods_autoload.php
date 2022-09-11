ac5a0453e45e63ca0de4e3ac6847cbb1231e29e2
Ninad Prabhune <prabhuneninad@gmail.com>
2022-09-11 20:55:34 +0530

    autoload updated

--- 14_3_magic_mathods_autoload.php
+++ 14_3_magic_mathods_autoload.php
@@ -3,6 +3,9 @@
 // Rule
 
 // require class name same as file name
+
+//This function has been DEPRECATED as of PHP 7.2. 0, and REMOVED as of PHP 8.0. 0.
+
 // require "classes/first.php";
 // require "classes/second.php";
 
