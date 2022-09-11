576e9d22e4945b12ebaf5b193cbe31de40423d91
Ninad Prabhune <prabhuneninad@gmail.com>
2022-09-11 19:39:41 +0530

    callstatic added

---
+++ 14_4_magic_mathods_call_static.php
@@ -0,0 +1,35 @@
+
+<!-- when you r trying to Call Non existing  or private Static Function directely outside class in give 
+undefined Method / fatal error message  -->
+<!-- to avoide this  error massage we  use __Clall method -->
+
+<!-- It automatically Call when You trying to Call non existing or private Static Function -->
+
+
+<!-- __Call method used to Cll private Static Function  from outside Class -->
+
+<?php
+
+
+class student{
+
+    
+
+    private static function hello(){
+
+        echo "This is Private static Method";
+    } 
+    
+    public static function __callstatic($method, $args){       
+
+        if(method_exists(__class__,$method)){
+            call_user_func_array([__class__,$method],$args);
+        }else{
+            echo " Method Doesnot Exist- '".$method."' <br>";
+        }
+    }   
+}
+   student::hello();
+   
+
+?>
---
+++ 14_4_magic_mathods_isset.php
@@ -0,0 +1,54 @@
+
+<!-- 
+    return 1 OR 0
+
+    Check value of variable set or not
+
+    It is a magic method that is invoked when isset() or empty() check non-existent or inaccessible class property:
+
+    __isset() is triggered by calling isset() or empty() on inaccessible (protected or private) or non-existing properties. 
+ -->
+
+<?php
+
+
+class student{
+
+    public $course;
+    private $first_name;
+    private $last_name;
+
+    public function setName($fname,$lname){
+         $this->first_name=$fname."<br>";
+         $this->last_name=$lname."<br>";
+    } 
+
+    public function __isset($property)
+    {
+       
+           echo isset($this->$property)."<br>";
+    }
+    
+    
+}
+   
+   $test = new student();
+
+
+//    $test->course='PHP';
+//    echo isset($test->course);
+
+
+//    $test->setName("Ninad", "Prabhune"); 
+   
+   
+   echo isset($test->first_name);
+   echo isset($test->last_name);
+
+
+   
+//     echo "<pre>";
+//     print_r($test);
+//     echo "</pre>";
+
+?>
