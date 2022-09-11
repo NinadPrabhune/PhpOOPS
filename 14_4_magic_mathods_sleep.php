e2d7df745f75f27398f758d8b69bf055dad86fa6
Ninad Prabhune <prabhuneninad@gmail.com>
2022-09-11 20:17:08 +0530

    sleep magic method added

---
+++ 14_4_magic_mathods_sleep.php
@@ -0,0 +1,34 @@
+<!-- 
+   sleep initilized before serialize function
+
+    sleep used to return limited property to array
+ -->
+
+<?php
+
+
+class student
+{
+
+   public $course = 'PHP';
+   private $first_name;
+   private $last_name;
+
+   public function setName($fname, $lname)
+   {
+      $this->first_name = $fname;
+      $this->last_name = $lname;
+   }
+
+   public function __sleep()
+   {
+      return array("first_name","last_name");
+   } 
+}
+
+$test = new student();
+$test->setName("Ninad", "Prabhune");
+$srl=serialize($test);
+var_dump($srl);
+
+?>
